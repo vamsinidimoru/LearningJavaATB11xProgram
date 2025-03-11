119ce84b2eed8246b11c7850d8c241f9136f8c53
VamsiNidimoru <nidimorus@gmail.com>
2025-03-11 15:38:18 +0530

    11th March

---
+++ src/printf.java
@@ -0,0 +1,28 @@
+public class printf {
+
+
+    public static void main(String[] args) {
+int table =9,a=1;
+
+        System.out.printf("%d*%d=%d",table,a,table*a);
+        System.out.println();
+        System.out.printf("%d*%d=%d",table,a+1,table*(a+1));
+        System.out.println();
+        System.out.printf("%d*%d=%d",table,a+2,table*(a+2));
+        System.out.println();
+        System.out.printf("%d*%d=%d",table,a+3,table*(a+3));
+        System.out.println();
+        System.out.printf("%d*%d=%d",table,a+4,table*(a+4));
+        System.out.println();
+        System.out.printf("%d*%d=%d",table,a+5,table*(a+5));
+        System.out.println();
+        System.out.printf("%d*%d=%d",table,a+6,table*(a+6));
+        System.out.println();
+        System.out.printf("%d*%d=%d",table,a+7,table*(a+7));
+        System.out.println();
+        System.out.printf("%d*%d=%d",table,a+8,table*(a+8));
+        System.out.println();
+        System.out.printf("%d*%d=%d",table,a+9,table*(a+9));
+    }
+
+}
