0b2928e843565f93bacb20c7020c0babf274bc90
VamsiNidimoru <nidimorus@gmail.com>
2025-03-12 12:35:41 +0530

    12th March

---
+++ src/mathfunctions.java
@@ -0,0 +1,6 @@
+public class mathfunctions {
+    public static void main(String[] args) {
+        double x=10,y=10,z=10;
+        System.out.println(Math.cbrt(Math.pow(x,2)+Math.pow(y,2)-Math.abs(z)));
+    }
+}
